821f58982a1e046a8fa7fa07372bc63b157e109f
Sebosun <sebos@protonmail.com>
2022-01-15 15:16:57 +0100

    loading button

--- src/components/Booking.tsx
+++ src/components/Booking.tsx
@@ -1,9 +1,25 @@
-import type { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import logo from "../../public/logo.svg";
 import "./Bookings.scss";
 
 export default function Booking(): ReactElement | null {
-  //todo imput
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+
+  const onUserInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setEmail(e.target.value);
+  };
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setLoading(true);
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+    setLoading(true);
+    setSubmitted(true);
+  }
+
   return (
     <div className="booking">
       <div className="booking__top">
@@ -12,20 +28,40 @@ export default function Booking(): ReactElement | null {
       </div>
       <div className="booking__contact">
         <img src={logo} alt="logo" />
-        <p>
-          Leave us your email address and we’ll let you know as soon as the
-          machine will start.
-        </p>
-        <form className="form">
-          <label htmlFor="email">Email</label>
+        {submitted ? (
+          <div className="booking__contact__submitted">
+            <h1>Your email has been submitted succesfully.</h1>
+          </div>
+        ) : (
+          <>
+            <p>
+              Leave us your email address and we’ll let you know as soon as the
+              machine will start.
+            </p>
+            <form onSubmit={(e) => handleSubmit(e)} className="form">
+              <label htmlFor="email">Email</label>
 
-          <input
-            type="email"
-            id="email"
-            placeholder="e.g. mynameislaura@mail.com"
-          />
-        </form>
-        <button className="btn">Let me know!</button>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={onUserInput}
+                placeholder="e.g. mynameislaura@mail.com"
+              />
+              <button className="btn">
+                {!loading ? (
+                  "Let me know!"
+                ) : (
+                  <div className="loading">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </div>
+                )}
+              </button>
+            </form>
+          </>
+        )}
       </div>
     </div>
   );
